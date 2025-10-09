9289ddbf6d1cb552b30fa0bd950d266030e1d35f
SayeddilawarG <sayed.dilawarg@gmail.com>
2025-10-09 15:02:32 +0530

    State variable and swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,35 @@
 import ResturantCard from "./ResturantCard";
-import restList from "../Utils/mockdata";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
+  const [list,setList] = useState([]);
 
-  return (
+  useEffect(()=>{
+    //API call
+    GetData();
+  },[])
+
+  const GetData = async () =>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1570854&lng=72.9966711&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    .then(async res=>{
+      const jsonForm = await res.json();
+      setList(jsonForm.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+  
+  return list.length ===0 ? <Shimmer/> : (
     <div className="body">
-      <div className="search-bar">
-        <input type="search" placeholder="Search for restaurants or dishes" />
+      <div className="filter">
+           <input type="search" placeholder="Search for restaurants or dishes" />
+           <button className="filter-btn" onClick={()=>{
+            const filteredList = list.filter(res=>res.info.avgRating>4);
+            setList(filteredList);
+           }}>Top Rated</button>
       </div>
       <div className="res-container">
-        {restList.map((resturant)=>(
+        {list.map((resturant)=>(
         <ResturantCard key={resturant.info.id} resData = {resturant.info}/>
         ))}
       </div>
